0356225a292c2fc913395cca6ce728ab1fc91d03
Josents <125105885+josentss@users.noreply.github.com>
2026-07-22 03:39:07 -0400

    Ajustando archivos extra

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}", // Esta línea es la clave
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-bg': '#0a0f16',
+        'dark-surface': '#131b26',
+        'dark-border': '#1e293b',
+        'accent-cyan': '#87ceeb',
+        'accent-yellow': '#FFF096',
+        'accent-green': '#00e054',
+      },
+    },
   },
   plugins: [],
-};
+}
